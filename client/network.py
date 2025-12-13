32f4714cd2503220cd52e79492f49e400fb2e8c8
koreapanda4444 <jun20091019@gmail.com>
2025-12-14 02:16:29 +0900

    TCP 양방향 프록시

--- client/network.py
+++ client/network.py
@@ -2,15 +2,18 @@ import json
 import threading
 import time
 import urllib.request
+import socket
+import select
 from state import state
 from config import config
+
+MC_PORT = 25565
 
 class NetworkManager:
     def __init__(self):
         self.running = False
-        self.poll_thread = None
-        self.peer_thread = None
         self.on_receive = lambda data: None
+        self.tcp_socket = None
 
     def _post(self, path, data):
         url = f"http://{config.oracle_host}:{config.oracle_port}{path}"
@@ -28,17 +31,22 @@ class NetworkManager:
             return
         self.running = True
         state.network_mode = "relay"
+        threading.Thread(target=self._relay_poll, daemon=True).start()
+        threading.Thread(target=self._peer_sync, daemon=True).start()
 
-        self.poll_thread = threading.Thread(target=self._poll_loop, daemon=True)
-        self.peer_thread = threading.Thread(target=self._peer_sync_loop, daemon=True)
-
-        self.poll_thread.start()
-        self.peer_thread.start()
+        if state.is_peer():
+            threading.Thread(target=self._tcp_peer, daemon=True).start()
+        if state.is_host():
+            threading.Thread(target=self._tcp_host, daemon=True).start()
 
     def disconnect(self):
         self.running = False
-        time.sleep(0.2)
         state.network_mode = None
+        try:
+            if self.tcp_socket:
+                self.tcp_socket.close()
+        except:
+            pass
 
     def send(self, data):
         if not self.running or not state.session_code:
@@ -51,27 +59,21 @@ class NetworkManager:
         except:
             pass
 
-    def _poll_loop(self):
+    def _relay_poll(self):
         while self.running:
-            if not state.session_code:
-                time.sleep(0.5)
-                continue
             try:
                 res = self._post("/relay/pull", {
                     "code": state.session_code
                 })
                 data = res.get("data")
-                if data is not None:
-                    self.on_receive(data)
+                if data:
+                    self._handle_packet(data)
             except:
                 pass
             time.sleep(0.05)
 
-    def _peer_sync_loop(self):
+    def _peer_sync(self):
         while self.running:
-            if not state.session_code:
-                time.sleep(1)
-                continue
             try:
                 res = self._post("/peers", {
                     "code": state.session_code
@@ -80,5 +82,64 @@ class NetworkManager:
             except:
                 pass
             time.sleep(2)
+
+    def _handle_packet(self, pkt):
+        if pkt.get("type") != "tcp":
+            self.on_receive(pkt)
+            return
+
+        if pkt.get("dir") == "host_to_peer" and state.is_peer():
+            try:
+                data = bytes.fromhex(pkt["data"])
+                if self.tcp_socket:
+                    self.tcp_socket.sendall(data)
+            except:
+                pass
+
+        if pkt.get("dir") == "peer_to_host" and state.is_host():
+            try:
+                data = bytes.fromhex(pkt["data"])
+                if self.tcp_socket:
+                    self.tcp_socket.sendall(data)
+            except:
+                pass
+
+    def _tcp_peer(self):
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        s.bind(("127.0.0.1", MC_PORT))
+        s.listen(1)
+        self.tcp_socket, _ = s.accept()
+        self.tcp_socket.setblocking(False)
+
+        while self.running:
+            r, _, _ = select.select([self.tcp_socket], [], [], 0.05)
+            if self.tcp_socket in r:
+                data = self.tcp_socket.recv(4096)
+                if not data:
+                    break
+                self.send({
+                    "type": "tcp",
+                    "dir": "peer_to_host",
+                    "data": data.hex()
+                })
+        s.close()
+
+    def _tcp_host(self):
+        self.tcp_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.tcp_socket.connect(("127.0.0.1", MC_PORT))
+        self.tcp_socket.setblocking(False)
+
+        while self.running:
+            r, _, _ = select.select([self.tcp_socket], [], [], 0.05)
+            if self.tcp_socket in r:
+                data = self.tcp_socket.recv(4096)
+                if not data:
+                    break
+                self.send({
+                    "type": "tcp",
+                    "dir": "host_to_peer",
+                    "data": data.hex()
+                })
+        self.tcp_socket.close()
 
 network = NetworkManager()
