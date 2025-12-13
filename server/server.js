f6032e8429995588f71e4298856f84b9f19f4bf7
koreapanda4444 <jun20091019@gmail.com>
2025-12-14 01:34:39 +0900

    Oracle Server 기본 구동

--- server/server.js
+++ server/server.js
@@ -1,7 +1,12 @@
 const http = require("http");
 
-http.createServer((req, res) => {
+const PORT = 3000;
+
+const server = http.createServer((req, res) => {
+  res.writeHead(200, { "Content-Type": "text/plain" });
   res.end("Minelink Oracle Server");
-}).listen(3000, () => {
-  console.log("Oracle server running on 3000");
+});
+
+server.listen(PORT, () => {
+  console.log(`Oracle server running on ${PORT}`);
 });
