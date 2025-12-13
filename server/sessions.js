1f678c77ca640129f44b10a8775ab186bf2c9ed3
koreapanda4444 <jun20091019@gmail.com>
2025-12-14 01:36:09 +0900

    Session & Join Code 관리

--- server/sessions.js
+++ server/sessions.js
@@ -1,9 +1,37 @@
 const sessions = {};
 
-function createSession() {
-  const code = Math.random().toString(36).substr(2, 6).toUpperCase();
-  sessions[code] = { peers: [] };
+function generateCode() {
+  return Math.random().toString(36).slice(2, 8).toUpperCase();
+}
+
+function createSession(hostId) {
+  let code;
+  do {
+    code = generateCode();
+  } while (sessions[code]);
+
+  sessions[code] = {
+    host: hostId,
+    peers: []
+  };
+
   return code;
 }
 
-module.exports = { createSession };
+function getSession(code) {
+  return sessions[code] || null;
+}
+
+function joinSession(code, peerId) {
+  const session = sessions[code];
+  if (!session) return false;
+
+  session.peers.push(peerId);
+  return true;
+}
+
+module.exports = {
+  createSession,
+  getSession,
+  joinSession
+};
